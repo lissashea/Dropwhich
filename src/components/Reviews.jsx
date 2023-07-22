e8fcbf6f0ce4ddf11dce231bb39230a50e7bdcd1
“Lissa <lwarshaw674@gmail.com>
2023-07-21 21:40:34 -0400

    reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -3,6 +3,7 @@ import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import review1 from "../review1.png";
 import review2 from "../review2.png";
+import "./Reviews.css"; // Make sure to import the Reviews.css file
 
 const Reviews = () => {
   const images = [review1, review2];
@@ -12,7 +13,10 @@ const Reviews = () => {
       <Slide>
         {images.map((image, index) => (
           <div key={index} className="each-slide">
-            <div style={{ backgroundImage: `url(${image})`, width: "100%", height: "600px", backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center",marginTop: "40px" }}></div>
+            <div style={{ backgroundImage: `url(${image})`, width: "100%", height: "600px", backgroundSize: "contain", backgroundRepeat: "no-repeat", backgroundPosition: "center",marginTop: "40px" }}>
+              {/* Add the text underneath the image */}
+              <p className="slide-text">This is a review for Image {index + 1}</p>
+            </div>
           </div>
         ))}
       </Slide>
